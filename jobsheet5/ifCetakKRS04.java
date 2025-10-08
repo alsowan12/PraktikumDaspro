8f9c7a05aa4560af37f92f46abda34b5291d60fe
alsowan12 <argawirawan485@gmail.com>
2025-10-08 16:27:08 +0700

    tugas 1

--- jobsheet5/ifCetakKRS04.java
+++ jobsheet5/ifCetakKRS04.java
@@ -10,12 +10,21 @@ public class ifCetakKRS04 {
         
         boolean uktLunas = input.nextBoolean();
 
+        // Versi IF-ELSE
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
         } else {
             System.out.println("Silakan lunasi UKT terlebih dahulu agar bisa mencetak KRS.");
         }
+
+        // 🔽 Tambahan: Versi Ternary Operator
+        String hasil = (uktLunas) 
+            ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+            : "Silakan lunasi UKT terlebih dahulu agar bisa mencetak KRS.";
+        
+        System.out.println("\n=== Versi Ternary Operator ===");
+        System.out.println(hasil);
 
         input.close();
     }
