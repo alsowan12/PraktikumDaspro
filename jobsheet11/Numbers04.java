587380188fcd8f4b08bb8a15b9d8f349bcd88b39
alsowan12 <argawirawan485@gmail.com>
2025-11-11 11:30:39 +0700

    update

---
+++ jobsheet11/Numbers04.java
@@ -0,0 +1,19 @@
+package jobsheet11;
+import java.util.Arrays;
+public class Numbers04 {
+    public static void main(String[] args){
+        int [][] Mynumbers;     
+        Mynumbers = new int [3][];
+        Mynumbers[0] = new int [5];
+        Mynumbers[1] = new int [3];
+        Mynumbers[2] = new int [1];
+         for (int i = 0; i < Mynumbers.length; i++) {
+            System.out.println(Arrays.toString(Mynumbers[i]));
+        }
+        for (int i = 0; i < Mynumbers.length; i++){
+            System.out.println("Baris ke-" + (i+1) + ":"+ Mynumbers[i].length);
+        }
+    }
+    
+    
+}
---
+++ jobsheet11/Siakad.java
@@ -0,0 +1,29 @@
+package jobsheet11;
+import java.util.Scanner;
+public class Siakad {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int [][] nilai = new int [4] [3];
+        for (int i = 0; i < nilai.length; i++){
+            System.out.println("Input Nilai Mahasiswa Ke-" + (i+1));
+            double totalPersiswa = 0;
+            for (int j = 0; j < nilai[i].length; j++){
+                System.out.print("Nilai Mata Kuliah ke-" + (j+1) + ": ");
+                nilai[i][j] = sc.nextInt();
+                totalPersiswa += nilai[i][j];
+            }
+            System.out.println("Nilai Rata-Rata: " + (totalPersiswa / 3));
+        }
+
+        System.out.println("\n=======================================");
+        System.out.println("Rata-rata Nilai Setiap Mata Kuliah");
+
+        for (int j = 0; j < 3; j++){
+            double totalPermatkul = 0;
+            for (int i = 0; i < 4; i++){
+                totalPermatkul += nilai[i][j];
+            }
+            System.out.println("Mata Kuliah " + (j+1) + ": " + (totalPermatkul / 4));
+        }
+    }
+}
