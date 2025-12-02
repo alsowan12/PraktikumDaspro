8dd9d376c344b14528732811e85fb116fe872352
alsowan12 <argawirawan485@gmail.com>
2025-12-02 13:05:46 +0700

    pertemuan 14

--- Pertemuan14/KafeDiskon04.java
+++ Pertemuan14/KafeDiskon04.java
@@ -58,6 +58,6 @@ public class KafeDiskon04{
     public static int hitungTotalHarga(int pilihanMenu, int banyakItem) { 
         int[] hargaItems = {15000, 20000, 22000, 12000, 12000, 18000};
         int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
-        return hargaTotal;    
+        return hargaTotal;
     } 
 }
